d1f1caae943800b1fd35a5645bef787d7eaf6953
Gavin <zheddie@163.com>
2016-09-30 22:23:23 +0800

    God save me.

--- src/glue/ObjectIterator.hpp
+++ src/glue/ObjectIterator.hpp
@@ -48,7 +48,7 @@ private:
 		/* Start _scanPtr after header */
 		//_scanPtr = (fomrobject_t *)objectPtr + 1;
 		_scanPtr = (fomrobject_t *)((char *)objectPtr  + sizeof(VMObject) -4);		//start from FIELDS[0]
-		MM_GCExtensionsBase *extensions = (MM_GCExtensionsBase *)omrVM->_gcOmrVMExtensions;
+		//MM_GCExtensionsBase *extensions = (MM_GCExtensionsBase *)omrVM->_gcOmrVMExtensions;
 		//uintptr_t size = extensions->objectModel.getConsumedSizeInBytesWithHeader(objectPtr);
 		//printf("zg.ObjectIterator.hpp.initialize().CP0,objectPtr=%p,size=%d\n",objectPtr,size);
 		//_endPtr = (fomrobject_t *)((U_8*)objectPtr + size);  //zg. TODO: Can not use this way. For example, VMSymbol, we can not use additional data as an reference to object.
